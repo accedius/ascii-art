a7b9dff410779cc65884f1fad5fb2c4bd7aa6a5f
Aleksei Baklanov <baklaal1@fit.cvut.cz>
2020-12-21 23:38:39 +0100

    Add more bound test for Image conversion test

--- src/test/scala/image/ImageTest.scala
+++ src/test/scala/image/ImageTest.scala
@@ -3,6 +3,28 @@ package image
 import org.scalatest.FunSuite
 
 class ImageTest extends FunSuite with ImageFactory {
+  test("Empty image") {
+    assertThrows[IllegalArgumentException](makeImageAsciiFromArray(Array.empty))
+    assertThrows[IllegalArgumentException](makeImageGrayscaleFromArray(Array.empty))
+  }
+
+  test("Row of zero length") {
+    assertThrows[IllegalArgumentException](makeImageAsciiFromArray(Array(
+      Array()
+    )))
+  }
+
+  test("Rows of a different length") {
+    assertThrows[IllegalArgumentException](makeImageGrayscaleFromArray(Array(
+      Array(0, 1),
+      Array(2))))
+  }
+
+  test("Grayscale image with invalid color values") {
+    assertThrows[IllegalArgumentException](makeImageGrayscaleFromArray(Array(Array(-1))))
+    assertThrows[IllegalArgumentException](makeImageGrayscaleFromArray(Array(Array(256))))
+  }
+
   test("Image to Ascii valid conversion") {
     val rgb = makeImageFromFile("src/test/resources/sonic.png")
     val greyscale = rgb.toGrayscale
