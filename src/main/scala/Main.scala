105554302542643bad8ff5d5f98b643b1ba8aab4
Aleksei Baklanov <baklaal1@fit.cvut.cz>
2020-12-21 23:03:40 +0100

    Update Main class to run program

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,11 +1,16 @@
 package Main
 
+import io.client.parser.CommandParser
+import io.client.CommandHandler
+
 object Main extends App {
-  println("Hello there")
-
-
-}
-
-object Foos {
-  def Foo() : Boolean = true
+  try {
+    val commands = CommandParser.parse(args)
+    CommandHandler(commands).run
+  } catch {
+    case thrown: Throwable => {
+      println(thrown.getMessage)
+      thrown.printStackTrace()
+    }
+  }
 }
